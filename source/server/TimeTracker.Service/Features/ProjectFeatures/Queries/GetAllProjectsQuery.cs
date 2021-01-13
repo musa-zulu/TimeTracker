6adb42377adb6000b1d506d4355666839b4c83b2
Musa <zuluchs@gmail.com>
2021-01-13 11:28:00 +0200

    added getallprojects query

---
+++ source/server/TimeTracker.Service/Features/ProjectFeatures/Queries/GetAllProjectsQuery.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using TimeTracker.Domain.Entities;
+using TimeTracker.Service.Contract;
+
+namespace TimeTracker.Service.Features.ProjectFeatures.Queries
+{
+    public class GetAllProjectsQuery : IRequest<IEnumerable<Project>>
+    {
+        public class GetAllProjectsQueryHandler : IRequestHandler<GetAllProjectsQuery, IEnumerable<Project>>
+        {
+            private readonly IProjectService _projectService;
+            public GetAllProjectsQueryHandler(IProjectService projectService)
+            {
+                _projectService = projectService;
+            }
+            public async Task<IEnumerable<Project>> Handle(GetAllProjectsQuery request, CancellationToken cancellationToken)
+            {
+                var projects = await _projectService.GetProjectsAsync();
+                if (projects == null)
+                {
+                    return null;
+                }
+                return projects?.Data ?? new List<Project>();
+            }
+        }
+    }
+}
