ab6156470a5c80f019f84c0b583e0af6705b3363
Musa <zuluchs@gmaail.com>
2021-08-20 16:12:03 +0200

    Tests
    Added more tests

---
+++ source/server/TimeTracker.Domain.Tests/Common/IpHelperTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System.Net;
+using TimeTracker.Domain.Common;
+
+namespace TimeTracker.Domain.Tests.Common
+{
+    [TestFixture]
+    public class IpHelperTests
+    {
+        public void GetIpAddress_GivenEmptyAddressList_ShouldReturnEmptyString()
+        {
+            //---------------Set up test pack-------------------
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            var results = IpHelper.GetIpAddress();
+            //---------------Test Result -----------------------
+            Assert.IsEmpty(results);
+        }
+
+        [Test]
+        public void GetIpAddress_GivenAddressList_ShouldReturnIpAddress()
+        {
+            //---------------Set up test pack-------------------
+            var hostName = "www.iamahost.com";
+            IPHostEntry host = Dns.Resolve(hostName);
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            var results = IpHelper.GetIpAddress();
+            //---------------Test Result -----------------------
+            Assert.IsNotEmpty(results);
+        }
+    }
+}
