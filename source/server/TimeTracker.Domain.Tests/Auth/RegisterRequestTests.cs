8a6279651a22f0ac93dada37858f0061c3153e78
Musa <zuluchs@gmaail.com>
2021-08-20 15:46:17 +0200

    Tests
    Added more tests

---
+++ source/server/TimeTracker.Domain.Tests/Auth/RegisterRequestTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using PeanutButter.TestUtils.Generic;
+using System;
+using TimeTracker.Domain.Auth;
+
+namespace TimeTracker.Domain.Tests.Auth
+{
+    [TestFixture]
+    public class RegisterRequestTests
+    {
+        [Test]
+        public void Construct()
+        {
+            //---------------Set up test pack-------------------
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            Assert.DoesNotThrow(() => new RegisterRequest());
+            //---------------Test Result -----------------------
+        }
+
+        [TestCase("FirstName", typeof(string))]
+        [TestCase("LastName", typeof(string))]
+        [TestCase("Email", typeof(string))]
+        [TestCase("UserName", typeof(string))]
+        [TestCase("Password", typeof(string))]
+        [TestCase("ConfirmPassword", typeof(string))]
+        public void RegisterRequest_ShouldHaveProperty(string propertyName, Type propertyType)
+        {
+            //---------------Set up test pack-------------------
+            var sut = typeof(RegisterRequest);
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            sut.ShouldHaveProperty(propertyName, propertyType);
+            //---------------Test Result -----------------------
+        }
+    }
+}
