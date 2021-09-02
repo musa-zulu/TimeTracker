2032d359e72b99fc0277c095a32441ef9869f884
Musa <zuluchs@gmaail.com>
2021-09-02 07:59:09 +0200

    added more tests

---
+++ source/server/TimeTracker.Domain.Tests/Dtos/ProjectDtoTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using PeanutButter.TestUtils.Generic;
+using System;
+using System.Collections.Generic;
+using TimeTracker.Domain.Dtos;
+
+namespace TimeTracker.Domain.Tests.Dtos
+{
+    [TestFixture]
+    public class ProjectDtoTests
+    {
+        [TestCase("ProjectId", typeof(Guid))]
+        [TestCase("Description", typeof(string))]
+        [TestCase("TotalHours", typeof(double))]
+        [TestCase("TotalBillableHours", typeof(double))]
+        [TestCase("TimeSlots", typeof(List<TimeSlotDto>))]
+        public void ProjectDto_ShouldHaveProperty(string propertyName, Type propertyType)
+        {
+            //---------------Set up test pack-------------------
+            var sut = typeof(ProjectDto);
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            sut.ShouldHaveProperty(propertyName, propertyType);
+            //---------------Test Result -----------------------
+        }
+
+    }
+}
