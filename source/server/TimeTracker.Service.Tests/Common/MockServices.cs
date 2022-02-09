a293da39b6061f47dc30c1ac96970514dcd3aeb3
AFRICA\103561 <celumusa.zulu@sasfin.com>
2022-02-09 13:28:59 +0200

    added tests

---
+++ source/server/TimeTracker.Service.Tests/Common/MockServices.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using TimeTracker.Domain.Common;
+using TimeTracker.Domain.Entities;
+using TimeTracker.Service.Contract;
+
+namespace TimeTracker.Service.Tests.Common
+{
+    public static class MockServices
+    {
+        public static Mock<ITaskService> GetTasks()
+        {
+            var tasks = new List<Task>
+            {
+                new Task
+                {
+                    TaskId = Guid.NewGuid(),
+                    Description = "new task"
+                }
+            };
+            var tasksResponse = new Response<List<Task>>
+            {
+                Data = tasks
+            };
+
+            var mockService = new Mock<ITaskService>();
+            mockService.Setup(x => x.GetTasksAsync()).ReturnsAsync(tasksResponse);
+            //mockService.Setup(x => x.AddTaskAsync(It.IsAny<Task>()))
+            //    .Returns(Task.FromResult(true));
+
+            return mockService;
+        }
+    }
+}
---
+++ source/server/TimeTracker.Service.Tests/Features/TaskFeature/Queries/GetAllTasksQueryTests.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TimeTracker.Domain.Common;
+using TimeTracker.Infrastructure.Mapping;
+using TimeTracker.Service.Contract;
+using TimeTracker.Service.Features.TaskFeatures.Queries;
+using TimeTracker.Service.Tests.Common;
+using static TimeTracker.Service.Features.TaskFeatures.Queries.GetAllTasksQuery;
+
+namespace TimeTracker.Service.Tests.Features.TaskFeature.Queries
+{
+    [TestFixture]
+    public class GetAllTasksQueryTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<ITaskService> _mockService;
+
+        public GetAllTasksQueryTests()
+        {
+            _mockService = MockServices.GetTasks();
+
+            var mapperConfig = new MapperConfiguration(c => {
+                c.AddProfile<TaskProfile>();
+            });
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Test]
+        public async Task GetTasksAsync_ShouldBeOfType_Task()
+        {
+            //---------------Set up test pack-------------------  
+            var handler = new GetAllTasksQueryHandler(_mockService.Object);
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            var result = await handler.Handle(new GetAllTasksQuery(), 
+                cancellationToken: System.Threading.CancellationToken.None);
+            //---------------Test Result -----------------------
+            result.Should().BeOfType<Response<List<Domain.Entities.Task>>>();
+        }
+
+    }
+}
