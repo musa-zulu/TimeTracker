84e671e4b428e1009c6afc3058a03e646a01ddda
Musa <zuluchs@gmail.com>
2023-09-30 08:03:00 +0200

    update TimeEntry Entity

--- source/server/TimeTracker.Domain/Entities/TimeEntry.cs
+++ source/server/TimeTracker.Domain/Entities/TimeEntry.cs
@@ -1,14 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TimeTracker.Domain.Entities
 {
     public class TimeEntry : BaseEntity
     {
         public Guid TimeEntryId { get; set; }
+        [Required]
         public Guid UserId { get; set; }
+        [Required]
         public Guid ProjectId { get; set; }
+        [Required]
         public Guid TaskId { get; set; }
+        [Required]
         public DateTime Date { get; set; }
+        [Required]
         public decimal HoursWorked { get; set; }
     }
 }
